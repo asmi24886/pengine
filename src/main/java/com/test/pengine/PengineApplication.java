0e5c623da5da7bedf5088070033a0fda742f816f
Asmit Basu <asmit@alumnux.com>
2020-09-08 21:18:03 +0530

    Promotion engine project setup commit

---
+++ src/main/java/com/test/pengine/PengineApplication.java
@@ -0,0 +1,13 @@
+package com.test.pengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/pengine/PengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.pengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
